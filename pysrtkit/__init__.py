e4577e30cff281e9503ca262438d9b95f5db5f40
kurianbenoy <kurian.bkk@gmail.com>
2025-06-09 22:48:34 +0530

    new version

--- pysrtkit/__init__.py
+++ pysrtkit/__init__.py
@@ -2,7 +2,7 @@ from pysrt.srttime import SubRipTime
 from pysrt.srtitem import SubRipItem
 from pysrt.srtfile import SubRipFile
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 open = SubRipFile.open
 stream = SubRipFile.stream
