d6976cdbf06b2c3ab9c2622c96268ca33d33816b
Kurian Benoy <kurian.bkk@gmail.com>
2025-06-09 22:46:55 +0530

    Update __init__.py

--- pysrtkit/__init__.py
+++ pysrtkit/__init__.py
@@ -1,1 +1,9 @@
+from pysrt.srttime import SubRipTime
+from pysrt.srtitem import SubRipItem
+from pysrt.srtfile import SubRipFile
+
 __version__ = "0.0.3"
+
+open = SubRipFile.open
+stream = SubRipFile.stream
+from_string = SubRipFile.from_string
